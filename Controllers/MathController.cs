7ac20f8581f472dd18cc1c285115a0ab18e18cb8
FilippoBuono <101173382+FilippoBuono@users.noreply.github.com>
2024-05-08 08:30:48 +0200

    Aggiunta del motedo per verificare se un valore è multilpo dell'altro

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -4,6 +4,29 @@ namespace AS2324_5G_INF_BuonoFilippo_WebAPI.Controllers
 {
     public class MathController : Controller
     {
+        [HttpGet("GetMultiplo")]
+        public JsonResult GetMultiplo(int num, int factor)
+        {
+            string status_result = "OK";
+            string message = "Problema";
+            if(num == 0)
+            {
+                status_result = "KO";
+            }
+            else
+            {
+                if(num % factor != 0)
+                {
+                    message = "non è un multiplo";
+                }
+                else
+                {
+                    message = "è un multiplo";
+                }
+            }
+            return Json(new { output = message, status = status_result });
+        }
+
         public IActionResult Index()
         {
             return View();
