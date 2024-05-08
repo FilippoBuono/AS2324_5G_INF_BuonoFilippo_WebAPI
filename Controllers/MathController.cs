7e40d6abf2b86b0ef17d120000de17269340db14
FilippoBuono <101173382+FilippoBuono@users.noreply.github.com>
2024-05-08 08:11:12 +0200

    Creazione del controller "MathController", pronto per lavorarci

---
+++ Controllers/MathController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_BuonoFilippo_WebAPI.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
